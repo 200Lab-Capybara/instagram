97e7c40e1f2b9bf5c280b20174a54e5d036999d7
hxuan190 <hxuan190@gmail.com>
2024-06-11 21:48:35 +0700

    add func random salt

---
+++ common/random_salt.go
@@ -0,0 +1,15 @@
+package common
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func GenerateRandomSalt(length int) (string, error) {
+	salt := make([]byte, length)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(salt), nil
+}
