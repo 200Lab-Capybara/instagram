d544d21d53e44e35ec93b4f6b0a0634fdf319052
NHT2-gh <thanhnn489@gmail.com>
2024-06-17 15:56:04 +0700

    update table notification delete the redundant payload field

--- db/notification.sql
+++ db/notification.sql
@@ -6,6 +6,6 @@ CREATE TABLE `notifications` (
     `from_user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `payload` json DEFAULT NULL,
-    `status` enum('read','sent','created') NOT NULL, `payload` json DEFAULT NULL,
+    `status` enum('read','sent','created') NOT NULL,
     UNIQUE KEY `id` (`id`)
 )
