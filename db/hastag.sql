e893dd530726b014abfcbd26ecf1617e5fb40fd8
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-17 18:57:26 +0700

    adding hastag table (#14)

--- db/hastag.sql
+++ db/hastag.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `hastag` (
   `id` varchar(36) NOT NULL,
-  `hashtag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `hashtag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `hastag_hashtag_idx` (`hashtag`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
