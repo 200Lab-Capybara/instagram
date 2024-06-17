abcaa143e178075b2e1eb2b760deee7cac402a59
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-17 16:31:56 +0700

    sql: add hastag table

---
+++ db/hastag.sql
@@ -0,0 +1,8 @@
+CREATE TABLE hashtags (
+    `id` varchar(36) NOT NULL,
+    `tag` VARCHAR(255) NOT NULL UNIQUE,
+    `post_id` varchar(36) NOT NULL,
+    `created_at` timestamp(6) NULL DEFAULT NULL,
+    `updated_at` timestamp(6) NULL DEFAULT NULL,
+
+);
