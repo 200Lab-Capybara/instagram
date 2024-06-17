83394a56f13074309cbaef548c059330d367657a
tuanloc-1708 <nguyenloc.170802@gmail.com>
2024-06-17 17:10:22 +0700

    Update profiles.sql

--- db/profiles.sql
+++ db/profiles.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `profiles` (
-    `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
     `date_of_birth` date DEFAULT NULL,
     `sex` enum('Male','Female','Other') NOT NULL DEFAULT 'Other',
     `avatar` json DEFAULT NULL,
     `count_following` int NOT NULL DEFAULT '0',
     `count_followers` int NOT NULL DEFAULT '0',
     `count_posts` int NOT NULL DEFAULT '0',
-    PRIMARY KEY (`user_id`)
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
