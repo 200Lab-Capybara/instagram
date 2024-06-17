eda17bdd984ccf262d14870640e81b0b035da39b
NHT2-gh <thanhnn489@gmail.com>
2024-06-17 15:30:25 +0700

    create table reaction_post in /db

---
+++ db/reaction_post.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `reaction_post` (
+    `post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`post_id`,`user_id`)
+)
