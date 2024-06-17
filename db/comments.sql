5a0bff11c097ea3203b49b7b2ef96f9dda45b9dd
tuanloc-1708 <nguyenloc.170802@gmail.com>
2024-06-17 16:56:19 +0700

    Update comments.sql

--- db/comments.sql
+++ db/comments.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `comments` (
-    `comment_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `created_at` datetime(6) DEFAULT NULL,
+    `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `created_at` datetime(6) NOT NULL,
     `updated_at` datetime(6) DEFAULT NULL,
-    `content` text,
-    `user_id` varchar(36) DEFAULT NULL,
-    PRIMARY KEY (`comment_id`)
+    `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `post_id` (`post_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
