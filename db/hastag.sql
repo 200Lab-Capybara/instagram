9110acea5dae30c077e8c5b05c93ff008e9347c6
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-17 16:56:41 +0700

    chore: fix hashtag table

--- db/hastag.sql
+++ db/hastag.sql
@@ -1,8 +1,7 @@
-CREATE TABLE hashtags (
-    `id` varchar(36) NOT NULL,
-    `tag` VARCHAR(255) NOT NULL UNIQUE,
-    `post_id` varchar(36) NOT NULL,
-    `created_at` timestamp(6) NULL DEFAULT NULL,
-    `updated_at` timestamp(6) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-);
+CREATE TABLE `hastag` (
+  `id` varchar(36) NOT NULL,
+  `hashtag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
