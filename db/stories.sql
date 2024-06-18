db825afa76882a9433be651f75eaedc492faf324
Mai Hoang Xuan <hxuan190@gmail.com>
2024-06-18 10:08:49 +0700

    change data type from datetime(6) to int

--- db/stories.sql
+++ db/stories.sql
@@ -5,7 +5,7 @@ create table stories
     user_id       varchar(36)   not null,
     content_story varchar(255)  null,
     react_count   int default 0 not null,
-    expires_time  datetime(6)   null,
+    expires_time  int           null,
     created_at    datetime(6)   null,
     updated_at    datetime(6)   null
 );
