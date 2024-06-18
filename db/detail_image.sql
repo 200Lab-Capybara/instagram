e3f37366bd4cae250f67c0b45c9c6f8b875ed64d
Mai Hoang Xuan <hxuan190@gmail.com>
2024-06-18 10:13:26 +0700

    change data type form timestamp to datetime(6)

--- db/detail_image.sql
+++ db/detail_image.sql
@@ -1,10 +1,10 @@
 create table image_detail
 (
-    model_name varchar(255)                        not null,
-    image_id   varchar(36)                         not null,
-    model_id   varchar(36)                         not null,
-    create_at  timestamp default CURRENT_TIMESTAMP not null,
-    updated_at timestamp                           null,
+    model_name varchar(255) not null,
+    image_id   varchar(36)  not null,
+    model_id   varchar(36)  not null,
+    create_at  datetime(6)  not null,
+    updated_at datetime(6)  null,
     primary key (image_id, model_id)
 );
 
