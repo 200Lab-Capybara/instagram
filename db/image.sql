0bb0b8313c7259e14df77f8a47e1ac4f11bfca57
Mai Hoang Xuan <104975107+hxuan190@users.noreply.github.com>
2024-06-18 10:45:43 +0700

    change data type table image & detail_image (#23)

--- db/image.sql
+++ db/image.sql
@@ -1,15 +1,15 @@
 create table images
 (
-    id           varchar(36)                         not null
+    id           varchar(36)             not null
         primary key,
-    image_url    varchar(255)                        not null,
-    size         int                                 not null,
-    width        int                                 not null,
-    height       int                                 not null,
-    status       enum ('used', 'unused')             not null,
-    create_at    timestamp default CURRENT_TIMESTAMP not null,
-    updated_at   timestamp                           null,
-    storage_name varchar(255)                        not null,
-    user_id      varchar(36)                         not null
+    image_url    varchar(255)            not null,
+    size         int                     not null,
+    width        int                     not null,
+    height       int                     not null,
+    status       enum ('used', 'unused') not null,
+    create_at    datetime(6)             not null,
+    updated_at   datetime(6)             null,
+    storage_name varchar(255)            not null,
+    user_id      varchar(36)             not null
 );
 
