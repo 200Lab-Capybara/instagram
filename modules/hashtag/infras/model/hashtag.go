545a69a333843005834661800d3c6458639f94ab
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-18 11:38:27 +0700

    feat: build a hastag struct and initialize file in_memory_repo file

---
+++ modules/hashtag/infras/model/hashtag.go
@@ -0,0 +1,14 @@
+
+package common
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Hashtag struct {
+	ID        uuid.UUID  `json:"id"`
+	Hashtag   string     `json:"hashtag"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+}
