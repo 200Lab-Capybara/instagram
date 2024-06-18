88e535a2673879e5d2cb13efeb2cd579622d3952
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-18 10:23:11 +0700

    Sql/hashtagpost (#21)
    * sql: add hastag table
    
    * feat: add primary key constraint to hashtags table
    
    * chore: fix hashtag table
    
    * sql: create hastag_post table
    
    * updating datetime
    
    * change port

--- db/hashtag_post.sql
+++ db/hashtag_post.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `hashtag_post` (
   `hashtag_id` varchar(36) NOT NULL,
   `post_id` varchar(36) NOT NULL,
-  `created_at` timestamp(6) NULL DEFAULT NULL,
-  `updated_at` timestamp(6) NULL DEFAULT NULL,
+  `created_at` datetime(6) NULL DEFAULT NULL,
+  `updated_at` datetime(6) NULL DEFAULT NULL,
   PRIMARY KEY (`hashtag_id`,`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ db/hastag.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `hastag` (
+  `id` varchar(36) NOT NULL,
+  `hashtag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
