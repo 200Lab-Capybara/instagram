d088782db10a43b12b8ac0e7b4d7b11bc188e053
Mai Hoang Xuan <hxuan190@gmail.com>
2024-06-18 09:46:09 +0700

    fix grammar & add updated_at

--- db/stories.sql
+++ db/stories.sql
@@ -1,12 +1,11 @@
 create table stories
 (
-    id               varchar(36)                         not null
+    id            varchar(36)                         not null
         primary key,
-    reaction_user_id varchar(36)                         not null,
-    text_story       varchar(255)                        null,
-    user_id          varchar(36)                         not null,
-    create_at        timestamp default CURRENT_TIMESTAMP null,
-    expires_at       timestamp                           null,
-    like_count       int       default 0                 not null
+    content_story varchar(255)                        null,
+    user_id       varchar(36)                         not null,
+    created_at    timestamp default CURRENT_TIMESTAMP null,
+    expires_time  timestamp                           null,
+    react_count   int       default 0                 not null
 );
 
