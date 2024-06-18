312a0d7f20126a11f3a01ae30d5ce16334e2b9cb
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-18 17:05:48 +0700

    feat: adding usecase for create hashtag

---
+++ app/internals/services/hashtag/usecase/createHashTag.go
@@ -0,0 +1,56 @@
+package hashtagusercase
+
+
+import (
+	"context"
+	"github.com/google/uuid"
+	hashtagmodel "github.com/nghiatrann0502/instagram-clone/app/internals/services/hashtag/model"
+)
+type createHashTagUseCase struct{
+	createHashTagRepository CreateHashTagRepository
+}
+
+func NewCreateHashTagUseCase(createHashTagRepository CreateHashTagRepository) CreateHashTagUseCase{
+	return &createHashTagUseCase{
+		createHashTagRepository : createHashTagRepository,
+	}
+}
+
+type CreateHashTagUseCase interface{
+	Execute(ctx context.Context, postId uuid.UUID, hashtag []string) ([]*hashtagmodel.HashtagPost, error)
+}
+
+type CreateHashTagRepository interface{
+	CheckNullHashTag(ctx context.Context, hashtag []string) ([]string, error)
+	GetHashTag(ctx context.Context, hashtag []string) ([]*hashtagmodel.Hashtag, error)
+	MapHashTag(ctx context.Context, hashtag *hashtagmodel.HashtagPost) (*hashtagmodel.HashtagPost,error)
+}
+
+func (u *createHashTagUseCase) Execute(ctx context.Context, postId uuid.UUID, hashtags []string) ([]*hashtagmodel.HashtagPost, error){
+	
+	nonNullHashtags, err := u.createHashTagRepository.CheckNullHashTag(ctx, hashtags)
+	if err != nil {
+		return nil, err
+	}
+
+	// Retrieve existing or create new hashtags
+	hashtagEntries, err := u.createHashTagRepository.GetHashTag(ctx, nonNullHashtags)
+	if err != nil {
+		return nil, err
+	}
+
+	// Map each hashtag to the postId
+	var mappedHashtags []*hashtagmodel.HashtagPost
+	for _, hashtag := range hashtagEntries {
+		mappedHashtag, err := u.createHashTagRepository.MapHashTag(ctx, &hashtagmodel.HashtagPost{
+			Hashtag_ID: hashtag.ID,
+			Post_ID:    postId,
+		})
+		if err != nil {
+			return nil, err
+		}
+		mappedHashtags = append(mappedHashtags, mappedHashtag)
+	}
+
+	return mappedHashtags, nil
+}
