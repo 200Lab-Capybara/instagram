d311f5b789555ace2c46de647a52f876c4288d7d
NghiaTran <nd.madlife@gmail.com>
2024-06-19 13:42:53 +0700

    Create base pubsub

---
+++ components/pubsub/pubsub.go
@@ -0,0 +1,29 @@
+package pubsub
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"time"
+)
+
+type MessageBroker interface {
+	Publish(ctx context.Context, topic string, msg *AppMessage) error
+	Subscribe(ctx context.Context, topic string) (subChan <-chan *AppMessage, close func(), err error)
+}
+
+type AppMessage struct {
+	Id        uuid.UUID
+	SenderId  *uuid.UUID
+	Topic     string
+	Payload   map[string]interface{}
+	CreatedAt time.Time
+}
+
+func NewAppMessage(senderId *uuid.UUID, topic string, payload map[string]interface{}) *AppMessage {
+	id, _ := uuid.NewV7()
+	return &AppMessage{Id: id, SenderId: senderId, Topic: topic, Payload: payload, CreatedAt: time.Now()}
+}
+
+func NewSimpleAppMessage(payload map[string]interface{}) *AppMessage {
+	return NewAppMessage(nil, "", payload)
+}
