af2466bf25bd4737df800e65ac9a12b92ac51a19
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-06-20 18:07:04 +0700

    feat: adding function respository

--- app/infras/services/hashtag/infras/repository/MapHashTag.go
+++ app/infras/services/hashtag/infras/repository/MapHashTag.go
@@ -1,36 +1,21 @@
 package hashtagsql
 
 import (
-    "context"
-    "database/sql"
-    "fmt"
-    "github.com/google/uuid"
-    hashtagmodel "github.com/nghiatrann0502/instagram-clone/app/internals/services/hashtag/model"
+	"context"
+	"github.com/google/uuid"
+	hashtagmodel "instagram/app/internals/services/hashtag/model"
 )
 
-type HashtagRepository struct {
-    DB *sql.DB
-}
-
 // MapHashTag maps a hashtag to a specific post in the database.
-func (repo *HashtagRepository) MapHashTag(ctx context.Context, hashtagPost *hashtagmodel.HashtagPost) (*hashtagmodel.HashtagPost, error) {
-    if hashtagPost == nil {
-        return nil, fmt.Errorf("hashtagPost cannot be nil")
-    }
+func (m *mySQLStorage) MapHashTag(ctx context.Context, postID uuid.UUID, hashtag hashtagmodel.Hashtag) (bool, error) {
+	db := m.db.GetConnection()
+	// Check if the hashtagPost is nil
 
-    // Prepare SQL statement for inserting or updating the post-hashtag relationship
-    stmt := `
-    INSERT INTO post_hashtags (hashtag_id, post_id) 
-    VALUES (?, ?) 
-    ON DUPLICATE KEY UPDATE 
-    hashtag_id = VALUES(hashtag_id), 
-    post_id = VALUES(post_id);
-    `
-    
-    _, err := repo.DB.ExecContext(ctx, stmt, hashtagPost.Hashtag_ID, hashtagPost.Post_ID)
-    if err != nil {
-        return nil, fmt.Errorf("failed to map hashtag to post: %w", err)
-    }
+	newHashTagPost := hashtagmodel.HashtagPost{Hashtag_ID: hashtag.ID, Post_ID: postID, CreatedAt: hashtag.CreatedAt}
 
-    return hashtagPost, nil
+	if err := db.Table(hashtagmodel.HashtagPost{}.TableName()).Create(&newHashTagPost).Error; err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
