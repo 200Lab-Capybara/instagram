dfe0a43c3ad84f28c6d7e448eed0bcab768ad8e0
Mai Hoang Xuan <hxuan190@gmail.com>
2024-06-25 15:37:55 +0700

    add(stories): add image_id to table stories

--- db/stories.sql
+++ db/stories.sql
@@ -1,12 +1,12 @@
-create table stories
+CREATE TABLE `stories`
 (
-    id            varchar(36)   not null
-        primary key,
-    user_id       varchar(36)   not null,
-    content_story varchar(255)  null,
-    react_count   int default 0 not null,
-    expires_time  int           null,
-    created_at    datetime(6)   null,
-    updated_at    datetime(6)   null
-);
-
+    `id`            varchar(36) NOT NULL,
+    `user_id`       varchar(36) NOT NULL,
+    `image_id`      varchar(36) NOT NULL,
+    `content_story` varchar(255)         DEFAULT NULL,
+    `react_count`   int         NOT NULL DEFAULT '0',
+    `expires_time`  int                  DEFAULT NULL,
+    `created_at`    datetime(6) DEFAULT NULL,
+    `updated_at`    datetime(6) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
