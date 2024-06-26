2b6ba94ea57a31e75ab2a2a1b369c4acd39b535c
NghiaTran <nd.madlife@gmail.com>
2024-06-26 22:43:40 +0700

    create api get feed

--- app/infras/services/posts/transport/http/post_handler.go
+++ app/infras/services/posts/transport/http/post_handler.go
@@ -8,16 +8,19 @@ import (
 type postHandler struct {
 	createPostUseCase postusecase.CreatePostUseCase
 	getPostsByUserId  postusecase.GetListPostByUserIdUseCase
+	getFeedUC         postusecase.GetFeedUseCase
 }
 
-func NewPostHandler(postUC postusecase.CreatePostUseCase, getPostsByUserId postusecase.GetListPostByUserIdUseCase) *postHandler {
+func NewPostHandler(postUC postusecase.CreatePostUseCase, getPostsByUserId postusecase.GetListPostByUserIdUseCase, getFeedUC postusecase.GetFeedUseCase) *postHandler {
 	return &postHandler{
 		createPostUseCase: postUC,
 		getPostsByUserId:  getPostsByUserId,
+		getFeedUC:         getFeedUC,
 	}
 }
 
 func (hdl *postHandler) RegisterV1Router(v1 *gin.RouterGroup, middleware gin.HandlerFunc) {
 	v1.POST("/posts", middleware, hdl.CreatePostHandler())
+	v1.GET("/posts", middleware, hdl.GetFeedHandler())
 	v1.GET("/posts/:userId/get-by-user", middleware, hdl.GetListPostByUserIdHandler())
 }
