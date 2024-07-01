df3bd82af4e715e227500dcefa8449edcc7c934c
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-07-01 12:10:46 +0700

    update: updating createhashtag function and maphashtag function

--- app/infras/services/hashtag/infras/repository/mysql/MapHashTag.go
+++ app/infras/services/hashtag/infras/repository/mysql/MapHashTag.go
@@ -7,14 +7,38 @@ import (
 )
 
 // MapHashTag maps a hashtag to a specific post in the database.
-func (m *mySQLStorage) MapHashTag(ctx context.Context, postID uuid.UUID, hashtag hashtagmodel.Hashtag) (bool, error) {
+//func (m *mySQLStorage) MapHashTag(ctx context.Context, postID uuid.UUID, hashtag []hashtagmodel.Hashtag) (bool, error) {
+//	var data hashtagmodel.HashtagPost
+//	db := m.db.GetConnection()
+//	// Check if the hashtagPost is nil
+//
+//	newHashTagPost := hashtagmodel.HashtagPost{Hashtag_ID: hashtag.ID, Post_ID: postID, CreatedAt: hashtag.CreatedAt}
+//
+//	if err := db.Table(data.TableName()).Create(&newHashTagPost).Error; err != nil {
+//		return false, err
+//	}
+//	return true, nil
+//}
+
+func (m *mySQLStorage) MapHashTag(ctx context.Context, postID uuid.UUID, hashtags []hashtagmodel.Hashtag) (bool, error) {
+	// Preallocate slice for all mappings to be inserted
 	var data hashtagmodel.HashtagPost
 	db := m.db.GetConnection()
-	// Check if the hashtagPost is nil
-	newHashTagPost := hashtagmodel.HashtagPost{Hashtag_ID: hashtag.ID, Post_ID: postID, CreatedAt: hashtag.CreatedAt}
+	hashtagPosts := make([]hashtagmodel.HashtagPost, len(hashtags))
 
-	if err := db.Table(data.TableName()).Create(&newHashTagPost).Error; err != nil {
-		return false, err
+	// Populate the slice using direct indexing
+	for i, hashtag := range hashtags {
+		hashtagPosts[i] = hashtagmodel.HashtagPost{
+			Hashtag_ID: hashtag.ID,
+			Post_ID:    postID,
+			CreatedAt:  hashtag.CreatedAt,
+		}
 	}
-	return true, nil
+
+	// Perform the batch insert using GORM
+	if err := db.Table(data.TableName()).Create(&hashtagPosts).Error; err != nil {
+		return false, err // Return false and the error if the batch insert fails
+	}
+
+	return true, nil // Return true if the batch insert is successful
 }
--- app/infras/services/hashtag/infras/repository/mysql/create_new_hashtag.go
+++ app/infras/services/hashtag/infras/repository/mysql/create_new_hashtag.go
@@ -6,17 +6,8 @@ import (
 	"instagram/common"
 )
 
-//type mySQLStorage struct {
-//	db common.SQLDatabase
-//}
-//
-//func NewMySQLStorage(db common.SQLDatabase) *mySQLStorage {
-//	return &mySQLStorage{
-//		db: db,
-//	}
-//}
+func (m *mySQLStorage) CreateHashTag(ctx context.Context, newHashTag []hashtagmodel.Hashtag) (bool, error) {
 
-func (m *mySQLStorage) CreateHashTag(ctx context.Context, newHashTag hashtagmodel.Hashtag) (bool, error) {
 	var data hashtagmodel.Hashtag
 	db := m.db.GetConnection()
 	if err := db.Table(data.TableName()).Create(&newHashTag).Error; err != nil {
