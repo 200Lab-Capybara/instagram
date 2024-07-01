a90f10b2f3d59a08614f8827975bec78d350e7d0
NghiaTran <nd.madlife@gmail.com>
2024-07-01 14:40:48 +0700

    fix

--- common/const.go
+++ common/const.go
@@ -1,36 +1,24 @@
 package common
 
 const (
-	CreatedPostTopic  = "created_post"
-	ReactedPostTopic  = "reacted_post"
-	FollowedUserTopic = "followed_user"
-	UnfollowUserTopic = "unfollow_user"
-  CreateCommentTopic  = "create_comment"
+	CreatedPostTopic    = "created_post"
+	ReactedPostTopic    = "reacted_post"
+	FollowedUserTopic   = "followed_user"
+	UnfollowUserTopic   = "unfollow_user"
+	CreateCommentTopic  = "create_comment"
 	ReactedCommentTopic = "reacted_comment"
-  CreatedStoryTopic = "created_story"
-	ReactedStoryTopic = "reacted_story"
+	CreatedStoryTopic   = "created_story"
+	ReactedStoryTopic   = "reacted_story"
+	CreatedImageTopic   = "created_image"
+	CreateStoryImage    = "story"
+	CreatePostImage     = "post"
 )
 
 const (
-	ReactedPostLike   = "like"
-	ReactedPostUnlike = "unlike"
-  ReactedCommentLike   = "like"
+	ReactedPostLike      = "like"
+	ReactedPostUnlike    = "unlike"
+	ReactedCommentLike   = "like"
 	ReactedCommentUnlike = "unlike"
-  ReactedStoryLike   = "like"
-	ReactedStoryUnlike = "unlike"
-)
-const (
-	ReactedStoryLike   = "like"
-	ReactedStoryUnlike = "unlike"
-)
-const (
-	CreatedStoryTopic = "created_story"
-	ReactedStoryTopic = "reacted_story"
-)
-const (
-	CreatedImageTopic = "created_image"
-)
-const (
-	CreateStoryImage = "story"
-	CreatePostImage  = "post"
+	ReactedStoryLike     = "like"
+	ReactedStoryUnlike   = "unlike"
 )
