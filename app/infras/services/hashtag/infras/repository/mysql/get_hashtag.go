720b67ca555fe55487f05c300357ca2953677f27
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-07-01 12:09:59 +0700

    ref: refractor

--- app/infras/services/hashtag/infras/repository/mysql/get_hashtag.go
+++ app/infras/services/hashtag/infras/repository/mysql/get_hashtag.go
@@ -5,26 +5,6 @@ import (
 	hashtagmodel "instagram/app/internals/services/hashtag/model"
 )
 
-// GetHashTags retrieves all individual hashtag entries from the database and appends each to a slice.
-//
-//	func (m *mySQLStorage) GetHashTags(ctx context.Context, hashtagSlice []hashtagmodel.Hashtag) ([]*hashtagmodel.Hashtag, error) {
-//		var hashtags []*hashtagmodel.Hashtag // Slice to hold the hashtags
-//
-//		if len(hashtagSlice) == 0 {
-//			return []*hashtagmodel.Hashtag{}, nil // Return nil and nil error if the slice is empty
-//		}
-//
-//		// Executes a query to fetch all records from the 'hashtags' table and append each to the 'hashtags' slice
-//		//if err := repo.db.GetConnection().Table("hashtags").Find(&hashtags).Error; err != nil {
-//		//	return nil, err // Return nil and the error if the query fails
-//		//}
-//
-//		if err := m.db.GetConnection().Table("hashtags").Where("hashtag IN ?", hashtagSlice).Find(&hashtags).Error; err != nil {
-//			return nil, err // Return nil and the error if the query fails
-//		}
-//
-//		return hashtags, nil // Return the slice containing all hashtags and nil error if successful
-//	}
 func (m *mySQLStorage) GetHashTags(ctx context.Context, hashtagSlice []string) ([]*hashtagmodel.Hashtag, error) {
 	var hashtag hashtagmodel.Hashtag
 	var hashtags []*hashtagmodel.Hashtag // Slice to hold hashtags from the database
@@ -38,13 +18,12 @@ func (m *mySQLStorage) GetHashTags(ctx context.Context, hashtagSlice []string) (
 	if err := m.db.GetConnection().Table(hashtag.TableName()).Where("Hashtag IN ?", hashtagSlice).Find(&hashtags).Error; err != nil {
 		return nil, err // Return nil and the error if the query fails
 	}
-
 	return hashtags, nil // Return the slice containing matched hashtags
 }
 
-// a, b, c
-// [a, d, e]
-// ? in (a, d, e)
+//a, b, c
+//[a, d, e]
+//? in (a, d, e)
 //{
 //	"a: true"
 //}
